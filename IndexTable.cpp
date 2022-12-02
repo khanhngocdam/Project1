c0094834cf5762e23dec3b31cd6c8a0a3a886898
Khanh <khanhngocdam>
2022-12-02 12:34:57 +0700

    Update file đầu vào bài toán chỉ mục

--- IndexTable.cpp
+++ IndexTable.cpp
@@ -3,13 +3,13 @@
 // SOICT - HUST
 #include <iostream>
 #include <fstream>
-#include "sll.cpp"
+#include "sll_hash.cpp"
 #include <cctype>
 #include <iomanip>
 #include "bns.cpp"
 
 using namespace std;
-
+// Kiểm tra xem chuỗi kí tự có phải 1 từ không 
 int isWord(char str[])
 {
     int i = 0;
@@ -24,12 +24,13 @@ int isWord(char str[])
     return 1;
 }
 
+
 void printMenu()
 {
     cout << "Data structure: \n";
     cout << "1. Single link list\n";
-    cout << "2. Hash table\n";
-    cout << "3. Binary tree\n";
+    cout << "2. Binary tree\n";
+    cout << "3. Hash table\n";
 }
 int main(int argc, char *argv[])
 {
@@ -61,17 +62,14 @@ int main(int argc, char *argv[])
             sll_push_w(list_stopw, str);
         }
         // lưu từ có ý nghĩa tra cứu vào danh sách liên kết
-        do
-        {
+        do {
             f1.get(c);
             // Đếm dòng
-            if (c == '\n')
-            {
+            if (c == '\n') {
                 count_line++;
             }
             // Nếu gặp  kí tự cách, xuống dòng hoặc kí tự kết thúc file thì lấy ra từ
-            if (c == ' ' || c == '\n' || f1.eof())
-            {
+            if (c == ' ' || c == '\n' || f1.eof()) {
                 str[j] = '\0';
                 if (isWord(str) && checkListString(list_stopw, str)) {
                     slnode_w *node_w = sll_push_w(list_w, str);
@@ -79,19 +77,16 @@ int main(int argc, char *argv[])
                 }
                 j = 0;
             }
-            else
-            {
+            else {
                 str[j] = tolower(c);
                 j++;
             }
         } while (!f1.eof());
         // Ghi vào file index.txt
-        for (slnode_w *p_w = list_w->front; p_w != NULL; p_w = p_w->next)
-        {
+        for (slnode_w *p_w = list_w->front; p_w != NULL; p_w = p_w->next) {
             f3 << setw(10) << left << p_w->word;
             f3 << setw(5) << left << p_w->count << "Line:   ";
-            for (slnode_l *p_l = p_w->list_line->front; p_l != NULL; p_l = p_l->next)
-            {
+            for (slnode_l *p_l = p_w->list_line->front; p_l != NULL; p_l = p_l->next) {
                 f3 << p_l->line << "  ";
             }
             f3 << "\n";
@@ -131,6 +126,44 @@ int main(int argc, char *argv[])
         } while(!f1.eof());
 // Ghi ra file
          writeFile(root_w, &f3);
+    }
+    if(choice == 3) {
+        char c, str[13];
+        // Bảng băm 
+        sllist_w** hash_list = new sllist_w*[275];
+        for (int i = 0; i < 275; i++) {
+            hash_list[i] = createList_w();
+        }
+        // List chứa các từ chỉ mục tra cứu
+        sllist_w *list_stopw = createList_w();
+        while (!f2.eof()) {
+            f2 >> str;
+            sll_push_w(list_stopw, str);
+        }
+        int count_line = 1, j = 0, index;
+        do {
+            f1.get(c);
+            // Đếm dòng
+            if (c == '\n') {
+                count_line++;
+            }
+            // Nếu gặp kí tự cách, xuống dòng hoặc kí tự kết thúc file thì lấy ra từ
+            if (c == ' ' || c == '\n' || f1.eof()) {
+                str[j] = '\0';
+                if (isWord(str) && checkListString(list_stopw, str)) {
+                    index = hash_index(str);
+                    slnode_w *node_w = sll_push_w(hash_list[index], str);
+                    sll_push_l(node_w -> list_line, count_line);
+
+                }
+                j = 0;
+            }
+            else {
+                str[j] = tolower(c);
+                j++;
+            }
+        } while (!f1.eof());
+        writeHashToFile(hash_list, &f3);
     }
     f1.close();
     f2.close();
--- test.cpp
+++ test.cpp
@@ -3,35 +3,6 @@
 #include<string>
 using namespace std;
 
-typedef struct node_tree {
-  node_tree* left;
-  node_tree* right;
-  int v;
-}node_tree;
-
-node_tree* createNodeTree(int value) {
-  node_tree* node = new node_tree;
-  node -> left = node -> right = NULL;
-  node -> v = value;
-  return node;
-}
- void insertTree(node_tree* &root, int value) {
-    
-    if(root != NULL) {
-        if(root -> v == value) {
-        } else if(root -> v < value) {
-             insertTree(root -> right, value);
-        } else {
-             insertTree(root -> left, value);
-        }
-    } else {
-      root = createNodeTree(value);
-    }
- }
 int main() {
-  node_tree* root = createNodeTree(12);
-  cout << root <<"  "<<root ->v<<endl;
-  cout << root -> right << root -> left <<endl;
-  insertTree(root, 15);
-  cout << root -> right->v ;
+  cout << 13 % 26 ;
 }
